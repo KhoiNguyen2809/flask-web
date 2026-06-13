46792fc44e04413855ad10661cf5914efe3329b5
KhoiNguyen2809 <nguyenvietkhoisn2012@gmail.com>
2026-06-13 17:59:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>📚 Thư viện Đề Tuyển Sinh</h1>"
+    return "📚 Thư viện Đề Tuyển Sinh"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
