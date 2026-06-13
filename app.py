1f5decbd3362bf0e9b6e605f6a227e20593d5c4a
KhoiNguyen2809 <nguyenvietkhoisn2012@gmail.com>
2026-06-13 20:06:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 import os
 
 app = Flask(__name__)
@@ -6,6 +7,33 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT * FROM users WHERE username=? AND password=?",
+            (username, password)
+        )
+
+        user = cursor.fetchone()
+
+        conn.close()
+
+        if user:
+            return f"Xin chào {username}!"
+
+        return "Sai tài khoản hoặc mật khẩu"
+
+    return render_template("login.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
